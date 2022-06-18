15b3fce16f735381b965bb44888a7ec282439afe
kawa <sRain97@163.com>
2022-06-18 20:16:53 +0800

    master-first

---
+++ src/main/java/com/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.git;
+
+/**
+ * @author kay
+ * @create 2022-06-18 19:22
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git---idea");
+    }
+}
