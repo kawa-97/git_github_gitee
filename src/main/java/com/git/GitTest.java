18051f2fac7bbcf0046a344783e701bb3ee1090e
kawa <sRain97@163.com>
2022-06-18 23:44:53 +0800

    master-merge

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git---idea");
         System.out.println("添加新分支");
+        System.out.println("融合成功");
     }
 }
